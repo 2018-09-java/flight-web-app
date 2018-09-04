a91ba8a1f06f14b5b2fdded38fdca9c0c18619e8
wbigger <wbigger@gmail.com>
2018-09-04 20:11:03 +0200

    Add simple ajax query to github

---
+++ MyFlight-html5/public_html/code.js
@@ -0,0 +1,28 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+
+var code = {
+    init: function () {
+        $.get("https://api.github.com/users/2018-09-java")
+                .done(code.onGetSuccessYeah)
+                .fail(code.onErrorBoooh);
+        
+    },
+    onGetSuccessYeah: function (dataJsonGithubApi) {
+        console.log(dataJsonGithubApi);
+        console.log("Type is: " + dataJsonGithubApi.type);
+        $(".page-heading").html(dataJsonGithubApi.login);
+        $(".page-heading").html(dataJsonGithubApi.login);
+    },
+    onErrorBoooh: function() {
+        console.log(":....(");
+    }
+};
+
+$(document).ready(code.init);
