bae2eac2d726cedd092e66863800f96977271143
wbigger <wbigger@gmail.com>
2018-09-07 11:05:29 +0200

    Make HTML5 page dynamic

--- MyFlight-html5/public_html/code.js
+++ MyFlight-html5/public_html/code.js
@@ -9,19 +9,38 @@
 
 var code = {
     init: function () {
-        $.get("http://localhost:8080/MyFlight-server/flights")
+        $.get("http://localhost:8080/MyFlight-server/webresources/flights")
                 .done(code.onGetSuccessYeah)
                 .fail(code.onErrorBoooh);
         
     },
-    onGetSuccessYeah: function (dataJsonGithubApi) {
-        console.log(dataJsonGithubApi);
-//        console.log("Type is: " + dataJsonGithubApi.type);
-//        $(".page-heading").html(dataJsonGithubApi.login);
-//        $(".page-heading").html(dataJsonGithubApi.login);
+    onGetSuccessYeah: function (dataJsonFlightApi) {
+        console.log(dataJsonFlightApi);
+        code.writeFlightList(dataJsonFlightApi.flights);
     },
     onErrorBoooh: function() {
         console.log(":....(");
+    },
+    writeFlightList: function(flightList) {
+        console.log("writeFlightList");
+        for (var f of flightList) {
+            console.log("f: "+f);
+            var htmlStr = `
+            <div class="flights-panel-row">
+                <div class="flights-panel-cell flights-panel-first-cell">
+                    <img class="company-logo" src="assets/az-logo.png" alt="company logo">
+                </div>
+                <div class="flights-panel-cell">
+                    <p>`+f.code+`</p>
+                    <p>
+                        <span>`+f.from+`</span>
+                        <span>-></span>
+                        <span>`+f.to+`</span>
+                    </p>
+                </div>
+            </div>`;
+            $("#flights-list").append(htmlStr);
+        }
     }
 };
 
